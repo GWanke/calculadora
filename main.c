6a2b67eda062c6bc67b81aaef30851c7d6f7f6b6
grwanke <ra91671@uem.br>
2018-09-12 16:42:17 -0300

    main implementada

--- main.c
+++ main.c
@@ -14,5 +14,18 @@ double div (double x, double y){
 	return x/y;
 }
 int main(){
+	int a,b;
+	char sinal;
+	scanf ("%d %c %d",&a, sinal, b);
+	if (sinal=="+")
+		soma(a,b);
+	else if (sinal=="-")
+		sub (a,b);
+	else if (sinal=="*")
+		mul (a,b);
+	else if (sinal=="/")
+		div (a,b);
+	else 
+		printf ("Sinal invalido");
 	return 0;
 }
